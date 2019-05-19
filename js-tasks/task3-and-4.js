931c0cdf1354b1cc23145de08a9122a6f73ff35c
MichalAW <michal.wanta@gmail.com>
2019-05-19 18:12:39 +0200

    change task3-and-4 to reduce

--- js-tasks/task3-and-4.js
+++ js-tasks/task3-and-4.js
@@ -1,11 +1,11 @@
-'use strict';
-
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 
 const average = (...args) => {
-  	const score = 0;
-  	const result = score.reduce((total, amount) => score += args[i]);
-  	result; //
+	let wynik = 0;
+	for (let i = 0; i < args.length; i++) {
+		wynik += args[i];
+	}
+	console.log(`Średnia to: ${wynik / args.length}`);
 };
 
 average(1); // 1
